4361a02f3062881e5ae03f55d9f516b29ce6bfc4
guojun.song <guojun.song@ptmind.com>
2018-04-25 19:17:07 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="layout" id="app">
+        <Layout>
+            <Header :style="{position: 'fixed',zIndex:'9999', top:'0', width: '100%',background:'#fff',boxShadow:'2px 2px 5px rgb(206, 205, 205)'}">
+                <Menu mode="horizontal" theme="light" active-name="1">
+                    <div class="layout-logo"></div>
+                    <div class="layout-nav" >
+                        <MenuItem name="1">
+                            <Icon type="ios-navigate"></Icon>
+                            首页
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="ios-keypad"></Icon>
+                            博客
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-analytics"></Icon>
+                            作品
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <Icon type="ios-paper"></Icon>
+                            大牛
+                        </MenuItem>
+                        <MenuItem name="5">
+                            登录/注册
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <Content :style="{margin: '88px 20px 0', background: '#fff', minHeight: '1000px'}">
+                <router-view/>
+            </Content>
+            <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+        </Layout>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style scoped>
+.layout-logo{
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+}
+.layout-nav{
+    width: 500px;
+    margin: 0 auto;
+    margin-right: 20px;
+}
+.layout-footer-center{
+    text-align: center;
+}
+.ivu-menu-light.ivu-menu-horizontal .ivu-menu-item-active, .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item:hover, .ivu-menu-light.ivu-menu-horizontal .ivu-menu-submenu-active, .ivu-menu-light.ivu-menu-horizontal .ivu-menu-submenu:hover {
+    color: #2d8cf0;
+    border-bottom:0;
+}
+.ivu-menu-light.ivu-menu-horizontal .ivu-menu-item, .ivu-menu-light.ivu-menu-horizontal .ivu-menu-submenu {
+    border-bottom: 0;
+}
+.ivu-menu-horizontal.ivu-menu-light:after {
+    background: #fff;
+}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,27 @@
+<template>      
+    <Carousel autoplay v-model="value2" loop :style="{height:'300px',background: '#506b9e'}">
+        <CarouselItem>
+            <div class="demo-carousel">1</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">2</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">3</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">4</div>
+        </CarouselItem>
+    </Carousel>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        value2:0
+      }
+    }
+  }
+</script>
